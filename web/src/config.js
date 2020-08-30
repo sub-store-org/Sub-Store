e1717459d4a6b5d2592151ab10e5a7a94098d441
Peng-YM <1048217874pengym@gmail.com>
2020-08-31 02:14:45 +0800

    Change Domain

--- web/src/config.js
+++ web/src/config.js
@@ -1,3 +1,3 @@
 const DEBUG = false;
 
-export const BACKEND_BASE = DEBUG ? `http://192.168.1.134:3000` : `https://sub.com`;
+export const BACKEND_BASE = DEBUG ? `http://192.168.1.134:3000` : `https://sub.store`;
