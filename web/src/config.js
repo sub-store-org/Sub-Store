2d6aa1ef1a38086de9573c9509170e2ce287880e
Peng-YM <1048217874pengym@gmail.com>
2020-09-01 17:56:24 +0800

    bug fixed

--- web/src/config.js
+++ web/src/config.js
@@ -1,2 +1,3 @@
-const DEBUG = process.env.NODE_ENV === "development";
+// const DEBUG = process.env.NODE_ENV === "development";
+const DEBUG = false;
 export const BACKEND_BASE = DEBUG ? `http://192.168.1.134:3000` : `https://sub.store`;
