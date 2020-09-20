17d514c6da4615898ac710568aa869e91822a836
Peng-YM <1048217874pengym@gmail.com>
2020-09-20 10:05:30 +0800

    Bug fixed

--- scripts/handle-duplicate.js
+++ scripts/handle-duplicate.js
@@ -2,8 +2,8 @@ function operator(proxies) {
     const counter = {};
     return proxies.map(p => {
       if (!counter[p.name]) counter[p.name] = 0;
-      const num = "00000" + counter[p.name];
       ++counter[p.name];
+      const num = "00000" + counter[p.name];
       p.name = p.name + " " + num.substr(num.length-2);
       return p;
     });
