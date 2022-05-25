e5c256803af350fdcfdf5635db91e7c4e9db7402
Peng-YM <1048217874pengym@gmail.com>
2022-05-25 18:17:08 +0800

    Refactored Loon resource parser

---
+++ backend/src/products/resource-parser.loon.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef */
+import { ProxyUtils } from '../core/proxy-utils';
+import { RuleUtils } from '../core/rule-utils';
+
+const RESOURCE_TYPE = {
+    PROXY: 1,
+    RULE: 2,
+};
+
+let result = $resource;
+
+if ($resourceType === RESOURCE_TYPE.PROXY) {
+    const proxies = ProxyUtils.parse($resource);
+    result = ProxyUtils.produce(proxies, 'Loon');
+} else if ($resourceType === RESOURCE_TYPE.RULE) {
+    const rules = RuleUtils.parse($resource);
+    result = RuleUtils.produce(rules, 'Loon');
+}
+
+$done(result);
