a9dc80cffaa010a63e80872c5314daa9bab39320
Peng-YM <1048217874pengym@gmail.com>
2022-05-25 18:34:02 +0800

    Minor fixes

--- scripts/vmess-ws-obfs-host.js
+++ scripts/vmess-ws-obfs-host.js
@@ -4,7 +4,7 @@
  * #host=google.com
  */
 function operator(proxies) {
-    const host = 'baidu.com';
+    const { host } = $arguments;
     proxies.forEach(p => {
         if (p.type === 'vmess' && p.network === 'ws') {
             p["ws-opts"] = p["ws-opts"] || {};
