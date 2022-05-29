6bf49c1de683e1cbf74b81e20796160fd6001e0a
Hsiaoyi Hsu <xreamxu@gmail.com>
2022-05-29 21:02:53 +0800

    Added support for custom domain

--- web/src/config.js
+++ web/src/config.js
@@ -1,4 +1,17 @@
 const DEBUG = process.env.NODE_ENV === 'development';
 const domain = process.env.DOMIAN || 'https://sub.store';
-export const BACKEND_BASE = DEBUG ? `http://localhost:3000` : domain;
+
+let savedDomain = localStorage.getItem('domain')
+
+const searchParams = new URLSearchParams(window.location.search)
+
+const newDomain = searchParams.get('domain')
+
+if (savedDomain !== newDomain) {
+    console.log(`newDomain: ${newDomain}`)
+    localStorage.setItem('domain', newDomain)
+    savedDomain = newDomain
+}
+
+export const BACKEND_BASE = savedDomain || (DEBUG ? `http://localhost:3000` : domain);
 // export const BACKEND_BASE = DEBUG ? `https://sub.store:9999` : `https://sub.store`;
