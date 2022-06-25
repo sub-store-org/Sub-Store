27c89a810fd4e8527b93df30fe8bfe370a09b6b0
qwerzl <46770502+qwerzl@users.noreply.github.com>
2022-06-25 21:59:30 +0800

    Changed editor

--- web/src/components/ScriptOperator.vue
+++ web/src/components/ScriptOperator.vue
@@ -53,7 +53,7 @@ import 'vue-prism-editor/dist/prismeditor.min.css';
 import { highlight, languages } from 'prismjs/components/prism-core';
 import 'prismjs/components/prism-clike';
 import 'prismjs/components/prism-javascript';
-import 'https://cdnjs.cloudflare.com/ajax/libs/prism-themes/1.9.0/prism-base16-ateliersulphurpool.light.min.css';
+import 'prism-themes/ateliersulphurpool-light.css';
 
 export default {
   components: {
