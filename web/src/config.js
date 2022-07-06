ca9ca76ad95b5b5bf03561927a1f82be6508bd3d
Peng-YM <1048217874pengym@gmail.com>
2022-07-06 15:41:01 +0800

    refactor: Add new frontend as submodule

--- web/src/config.js
+++
@@ -1,4 +0,0 @@
-const DEBUG = process.env.NODE_ENV === 'development';
-const domain = process.env.DOMIAN || 'https://sub.store';
-export const BACKEND_BASE = DEBUG ? `http://localhost:3000` : domain;
-// export const BACKEND_BASE = DEBUG ? `https://sub.store:9999` : `https://sub.store`;
