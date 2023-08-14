d2290477444572af700634fcdd9dbc777c916a3b
xream <xream@icloud.com>
2023-08-14 10:04:47 +0800

    Fixed: unsupported cipher for Clash/Stash

--- backend/src/core/proxy-utils/producers/stash.js
+++ backend/src/core/proxy-utils/producers/stash.js
@@ -28,6 +28,19 @@ export default function Stash_Producer() {
                             proxy.servername = proxy.sni;
                             delete proxy.sni;
                         }
+                        // https://github.com/MetaCubeX/Clash.Meta/blob/Alpha/docs/config.yaml#L400
+                        // https://stash.wiki/proxy-protocols/proxy-types#vmess
+                        if (
+                            isPresent(proxy, 'cipher') &&
+                            ![
+                                'auto',
+                                'aes-128-gcm',
+                                'chacha20-poly1305',
+                                'none',
+                            ].includes(proxy.cipher)
+                        ) {
+                            proxy.cipher = 'auto';
+                        }
                     } else if (proxy.type === 'tuic') {
                         if (isPresent(proxy, 'alpn')) {
                             proxy.alpn = Array.isArray(proxy.alpn)
