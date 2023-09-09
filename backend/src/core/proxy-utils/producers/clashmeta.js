939022e5a3921ce17ed2158c7e2d20b0687d2d9a
xream <xream@icloud.com>
2023-09-09 14:03:40 +0800

    fix: 修复了 Clash.Meta 输出 VLESS 时 内部字段 sni 未作用到 servername 的问题

--- backend/src/core/proxy-utils/producers/clashmeta.js
+++ backend/src/core/proxy-utils/producers/clashmeta.js
@@ -81,6 +81,11 @@ export default function ClashMeta_Producer() {
                         proxy['preshared-key'] =
                             proxy['preshared-key'] ?? proxy['pre-shared-key'];
                         proxy['pre-shared-key'] = proxy['preshared-key'];
+                    } else if (proxy.type === 'vless') {
+                        if (isPresent(proxy, 'sni')) {
+                            proxy.servername = proxy.sni;
+                            delete proxy.sni;
+                        }
                     }
 
                     if (
