3314ad5f0e12ba0266a8551f29a38df96fa72f87
xream <xream@icloud.com>
2023-11-14 21:46:56 +0800

    feat: Sub-Store 生成的订阅地址支持传入 订阅链接/User-Agent/节点内容 可以复用此订阅的其他设置
    例如: 建一个 name 为 sub 的订阅, 配置好节点操作
    
    以后可以自由传入参数 无需在 Sub-Store 前端创建新的配置
    
    `/download/sub?target=Surge&content=encodeURIComponent编码过的本地节点`
    
    `/download/sub?target=Surge&url=encodeURIComponent编码过的订阅链接&ua=encodeURIComponent编码过的User-Agent`

--- backend/src/restful/download.js
+++ backend/src/restful/download.js
@@ -20,6 +20,16 @@ async function downloadSubscription(req, res) {
         req.query.target || getPlatformFromHeaders(req.headers) || 'JSON';
 
     $.info(`正在下载订阅：${name}`);
+    const { url, ua, content } = req.query;
+    if (url) {
+        $.info(`指定 url: ${url}`);
+    }
+    if (ua) {
+        $.info(`指定 ua: ${ua}`);
+    }
+    if (content) {
+        $.info(`指定 content: ${content}`);
+    }
 
     const allSubs = $.read(SUBS_KEY);
     const sub = findByName(allSubs, name);
@@ -29,12 +39,15 @@ async function downloadSubscription(req, res) {
                 type: 'subscription',
                 name,
                 platform,
+                url,
+                ua,
+                content,
             });
 
-            if (sub.source !== 'local') {
+            if (sub.source !== 'local' || url) {
                 try {
                     // forward flow headers
-                    const flowInfo = await getFlowHeaders(sub.url);
+                    const flowInfo = await getFlowHeaders(url || sub.url);
                     if (flowInfo) {
                         res.set('subscription-userinfo', flowInfo);
                     }
