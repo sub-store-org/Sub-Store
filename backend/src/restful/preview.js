e21a25e8f45f9043f176906348e78ed5ebd7fc4c
xream <xream@icloud.com>
2023-12-18 01:24:48 +0800

    feat: 支持忽略失败的远程订阅(前端版本 > 2.14.20)

--- backend/src/restful/preview.js
+++ backend/src/restful/preview.js
@@ -22,24 +22,31 @@ async function compareSub(req, res) {
         ) {
             content = sub.content;
         } else {
-            try {
-                content = await Promise.all(
-                    sub.url
-                        .split(/[\r\n]+/)
-                        .map((i) => i.trim())
-                        .filter((i) => i.length)
-                        .map((url) => download(url, sub.ua)),
+            const errors = {};
+            content = await Promise.all(
+                sub.url
+                    .split(/[\r\n]+/)
+                    .map((i) => i.trim())
+                    .filter((i) => i.length)
+                    .map(async (url) => {
+                        try {
+                            return await download(url, sub.ua);
+                        } catch (err) {
+                            errors[url] = err;
+                            $.error(
+                                `订阅 ${sub.name} 的远程订阅 ${url} 发生错误: ${err}`,
+                            );
+                            return '';
+                        }
+                    }),
+            );
+
+            if (!sub.ignoreFailedRemoteSub && Object.keys(errors).length > 0) {
+                throw new Error(
+                    `订阅 ${sub.name} 的远程订阅 ${Object.keys(errors).join(
+                        ', ',
+                    )} 发生错误, 请查看日志`,
                 );
-            } catch (err) {
-                failed(
-                    res,
-                    new NetworkError(
-                        'FAILED_TO_DOWNLOAD_RESOURCE',
-                        '无法下载远程资源',
-                        `Reason: ${err}`,
-                    ),
-                );
-                return;
             }
             if (sub.mergeSources === 'localFirst') {
                 content.unshift(sub.content);
@@ -87,69 +94,95 @@ async function compareCollection(req, res) {
         const collection = req.body;
         const subnames = collection.subscriptions;
         const results = {};
-        let hasError;
+        const errors = {};
         await Promise.all(
             subnames.map(async (name) => {
-                if (!hasError) {
-                    const sub = findByName(allSubs, name);
-                    try {
-                        let raw;
-                        if (
-                            sub.source === 'local' &&
-                            !['localFirst', 'remoteFirst'].includes(
-                                sub.mergeSources,
-                            )
-                        ) {
-                            raw = sub.content;
-                        } else {
-                            raw = await Promise.all(
-                                sub.url
-                                    .split(/[\r\n]+/)
-                                    .map((i) => i.trim())
-                                    .filter((i) => i.length)
-                                    .map((url) => download(url, sub.ua)),
-                            );
-                            if (sub.mergeSources === 'localFirst') {
-                                raw.unshift(sub.content);
-                            } else if (sub.mergeSources === 'remoteFirst') {
-                                raw.push(sub.content);
-                            }
-                        }
-                        // parse proxies
-                        let currentProxies = (Array.isArray(raw) ? raw : [raw])
-                            .map((i) => ProxyUtils.parse(i))
-                            .flat();
-
-                        currentProxies.forEach((proxy) => {
-                            proxy.subName = sub.name;
-                            proxy.collectionName = collection.name;
-                        });
-
-                        // apply processors
-                        currentProxies = await ProxyUtils.process(
-                            currentProxies,
-                            sub.process || [],
-                            'JSON',
-                            { [sub.name]: sub, _collection: collection },
+                const sub = findByName(allSubs, name);
+                try {
+                    let raw;
+                    if (
+                        sub.source === 'local' &&
+                        !['localFirst', 'remoteFirst'].includes(
+                            sub.mergeSources,
+                        )
+                    ) {
+                        raw = sub.content;
+                    } else {
+                        const errors = {};
+                        raw = await Promise.all(
+                            sub.url
+                                .split(/[\r\n]+/)
+                                .map((i) => i.trim())
+                                .filter((i) => i.length)
+                                .map(async (url) => {
+                                    try {
+                                        return await download(url, sub.ua);
+                                    } catch (err) {
+                                        errors[url] = err;
+                                        $.error(
+                                            `订阅 ${sub.name} 的远程订阅 ${url} 发生错误: ${err}`,
+                                        );
+                                        return '';
+                                    }
+                                }),
                         );
-                        results[name] = currentProxies;
-                    } catch (err) {
-                        if (!hasError) {
-                            hasError = true;
-                            failed(
-                                res,
-                                new InternalServerError(
-                                    'PROCESS_FAILED',
-                                    `处理子订阅 ${name} 失败`,
-                                    `Reason: ${err}`,
-                                ),
+                        if (
+                            !sub.ignoreFailedRemoteSub &&
+                            Object.keys(errors).length > 0
+                        ) {
+                            throw new Error(
+                                `订阅 ${sub.name} 的远程订阅 ${Object.keys(
+                                    errors,
+                                ).join(', ')} 发生错误, 请查看日志`,
                             );
+                        }
+                        if (sub.mergeSources === 'localFirst') {
+                            raw.unshift(sub.content);
+                        } else if (sub.mergeSources === 'remoteFirst') {
+                            raw.push(sub.content);
                         }
                     }
+                    // parse proxies
+                    let currentProxies = (Array.isArray(raw) ? raw : [raw])
+                        .map((i) => ProxyUtils.parse(i))
+                        .flat();
+
+                    currentProxies.forEach((proxy) => {
+                        proxy.subName = sub.name;
+                        proxy.collectionName = collection.name;
+                    });
+
+                    // apply processors
+                    currentProxies = await ProxyUtils.process(
+                        currentProxies,
+                        sub.process || [],
+                        'JSON',
+                        { [sub.name]: sub, _collection: collection },
+                    );
+                    results[name] = currentProxies;
+                } catch (err) {
+                    errors[name] = err;
+
+                    $.error(
+                        `❌ 处理组合订阅中的子订阅: ${
+                            sub.name
+                        }时出现错误：${err}！进度--${
+                            100 * (processed / subnames.length).toFixed(1)
+                        }%`,
+                    );
                 }
             }),
         );
-        if (hasError) return;
+        if (
+            !collection.ignoreFailedRemoteSub &&
+            Object.keys(errors).length > 0
+        ) {
+            throw new Error(
+                `组合订阅 ${collection.name} 中的子订阅 ${Object.keys(
+                    errors,
+                ).join(', ')} 发生错误, 请查看日志`,
+            );
+        }
         // merge proxies with the original order
         const original = Array.prototype.concat.apply(
             [],
