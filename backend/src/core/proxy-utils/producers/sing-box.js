779950ab11aed4851a29a3100126809d812133eb
xream <xream@icloud.com>
2024-01-18 11:36:07 +0800

    Revert "fix: sing-box fingerprint"
    This reverts commit 42404537e8e6a7f5590cda3e5de5839b9f2dcddd.

--- backend/src/core/proxy-utils/producers/sing-box.js
+++ backend/src/core/proxy-utils/producers/sing-box.js
@@ -205,11 +205,6 @@ const tlsParser = (proxy, parsedProxy) => {
             enabled: true,
             fingerprint: proxy['client-fingerprint'],
         };
-    if (proxy.fingerprint && proxy.fingerprint !== '')
-        parsedProxy.tls.utls = {
-            enabled: true,
-            fingerprint: proxy.fingerprint,
-        };
     if (proxy['reality-opts']) {
         parsedProxy.tls.reality = { enabled: true };
         if (proxy['reality-opts']['public-key'])
