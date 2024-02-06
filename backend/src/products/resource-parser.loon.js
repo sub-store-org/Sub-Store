47f26bdac86a0a1e67c8ff5faf394aff937ad530
xream <xream@icloud.com>
2024-02-06 21:29:10 +0800

    feat: Loon 解析器支持参数 ua=clash.meta&timeout=3000, 支持从链接重新获取

--- backend/src/products/resource-parser.loon.js
+++ backend/src/products/resource-parser.loon.js
@@ -2,28 +2,79 @@
 import { ProxyUtils } from '@/core/proxy-utils';
 import { RuleUtils } from '@/core/rule-utils';
 import { version } from '../../package.json';
+import download from '@/utils/download';
 
-console.log(
-    `
-┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅
-     Sub-Store -- v${version}
-┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅
-`,
-);
+let result = '';
+let resource = typeof $resource !== 'undefined' ? $resource : '';
+let resourceType = typeof $resourceType !== 'undefined' ? $resourceType : '';
+let resourceUrl = typeof $resourceUrl !== 'undefined' ? $resourceUrl : '';
 
-const RESOURCE_TYPE = {
-    PROXY: 1,
-    RULE: 2,
-};
+!(async () => {
+    console.log(
+        `
+    ┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅
+         Sub-Store -- v${version}
+    ┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅
+    `,
+    );
 
-let result = $resource;
+    let arg;
+    if (typeof $argument != 'undefined') {
+        arg = Object.fromEntries(
+            $argument.split('&').map((item) => item.split('=')),
+        );
+    } else {
+        arg = {};
+    }
 
-if ($resourceType === RESOURCE_TYPE.PROXY) {
-    const proxies = ProxyUtils.parse($resource);
-    result = ProxyUtils.produce(proxies, 'Loon');
-} else if ($resourceType === RESOURCE_TYPE.RULE) {
-    const rules = RuleUtils.parse($resource);
-    result = RuleUtils.produce(rules, 'Loon');
-}
+    const RESOURCE_TYPE = {
+        PROXY: 1,
+        RULE: 2,
+    };
 
-$done(result);
+    result = resource;
+
+    if (resourceType === RESOURCE_TYPE.PROXY) {
+        try {
+            let proxies = ProxyUtils.parse(resource);
+            result = ProxyUtils.produce(proxies, 'Loon');
+        } catch (e) {
+            console.log('解析器: 使用 resource 出现错误');
+            console.log(e.message ?? e);
+        }
+        if ((!result || /^\s*$/.test(result)) && resourceUrl) {
+            console.log(`解析器: 尝试从 ${resourceUrl} 获取订阅`);
+            try {
+                let raw = await download(resourceUrl, arg?.ua, arg?.timeout);
+                let proxies = ProxyUtils.parse(raw);
+                result = ProxyUtils.produce(proxies, 'Loon');
+            } catch (e) {
+                console.log(e.message ?? e);
+            }
+        }
+    } else if (resourceType === RESOURCE_TYPE.RULE) {
+        try {
+            const rules = RuleUtils.parse(resource);
+            result = RuleUtils.produce(rules, 'Loon');
+        } catch (e) {
+            console.log(e.message ?? e);
+        }
+        if ((!result || /^\s*$/.test(result)) && resourceUrl) {
+            console.log(`解析器: 尝试从 ${resourceUrl} 获取规则`);
+            try {
+                let raw = await download(resourceUrl, arg?.ua, arg?.timeout);
+                let rules = RuleUtils.parse(raw);
+                result = RuleUtils.produce(rules, 'Loon');
+            } catch (e) {
+                console.log(e.message ?? e);
+            }
+        }
+    }
+})()
+    .catch(async (e) => {
+        console.log('解析器: 出现错误');
+        console.log(e.message ?? e);
+    })
+    .finally(() => {
+        $done(result || '');
+    });
