0ccd0e5451d8b02192049b0c181461c0f394f3b2
xream <xream@icloud.com>
2024-03-08 18:40:44 +0800

    feat: 订阅列表的流量信息兼容远程和本地合并的情况, 排除设置了不查询订阅信息的链接

--- backend/src/restful/subscriptions.js
+++ backend/src/restful/subscriptions.js
@@ -43,7 +43,10 @@ async function getFlowInfo(req, res) {
         );
         return;
     }
-    if (sub.source === 'local') {
+    if (
+        sub.source === 'local' &&
+        !['localFirst', 'remoteFirst'].includes(sub.mergeSources)
+    ) {
         failed(
             res,
             new RequestInvalidError(
@@ -55,7 +58,42 @@ async function getFlowInfo(req, res) {
         return;
     }
     try {
-        const flowHeaders = await getFlowHeaders(sub.url);
+        let url = `${sub.url}`
+            .split(/[\r\n]+/)
+            .map((i) => i.trim())
+            .filter((i) => i.length)?.[0];
+
+        let $arguments = {};
+        const rawArgs = url.split('#');
+        url = url.split('#')[0];
+        if (rawArgs.length > 1) {
+            try {
+                // 支持 `#${encodeURIComponent(JSON.stringify({arg1: "1"}))}`
+                $arguments = JSON.parse(decodeURIComponent(rawArgs[1]));
+            } catch (e) {
+                for (const pair of rawArgs[1].split('&')) {
+                    const key = pair.split('=')[0];
+                    const value = pair.split('=')[1];
+                    // 部分兼容之前的逻辑 const value = pair.split('=')[1] || true;
+                    $arguments[key] =
+                        value == null || value === ''
+                            ? true
+                            : decodeURIComponent(value);
+                }
+            }
+        }
+        if ($arguments.noFlow) {
+            failed(
+                res,
+                new RequestInvalidError(
+                    'NO_FLOW_INFO',
+                    'N/A',
+                    `Subscription ${name}: noFlow`,
+                ),
+            );
+            return;
+        }
+        const flowHeaders = await getFlowHeaders(url);
         if (!flowHeaders) {
             failed(
                 res,
