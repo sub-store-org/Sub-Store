23cf81d0a59e4fc7728851632d3e7f83b18b6eb7
xream <xream@icloud.com>
2024-03-08 14:20:55 +0800

    feat: Node.js 版 /api/utils/env 增加 meta 信息

--- backend/src/utils/env.js
+++ backend/src/utils/env.js
@@ -36,6 +36,21 @@ try {
         // eslint-disable-next-line no-undef
         meta.script = $script;
     }
+    if (isNode) {
+        meta.node = {
+            version: eval('process.version'),
+            argv: eval('process.argv'),
+            filename: eval('__filename'),
+            dirname: eval('__dirname'),
+            env: {},
+        };
+        const env = eval('process.env');
+        for (const key in env) {
+            if (/^SUB_STORE_/.test(key)) {
+                meta.node.env[key] = env[key];
+            }
+        }
+    }
     // eslint-disable-next-line no-empty
 } catch (e) {}
 
