520e3f9c1549d3165ebd4399ddbd2223e74598ad
xream <xream@icloud.com>
2024-07-19 14:33:34 +0800

    fix: 修复 URI 报错

--- backend/src/core/proxy-utils/producers/v2ray.js
+++ backend/src/core/proxy-utils/producers/v2ray.js
@@ -1,12 +1,30 @@
 /* eslint-disable no-case-declarations */
 import { Base64 } from 'js-base64';
 import URI_Producer from './uri';
+import $ from '@/core/app';
 
 const URI = URI_Producer();
 
 export default function V2Ray_Producer() {
     const type = 'ALL';
-    const produce = (proxies) =>
-        Base64.encode(proxies.map((proxy) => URI.produce(proxy)).join('\n'));
+    const produce = (proxies) => {
+        let result = [];
+        proxies.map((proxy) => {
+            try {
+                result.push(URI.produce(proxy));
+            } catch (err) {
+                $.error(
+                    `Cannot produce proxy: ${JSON.stringify(
+                        proxy,
+                        null,
+                        2,
+                    )}\nReason: ${err}`,
+                );
+            }
+        });
+
+        return Base64.encode(result.join('\n'));
+    };
+
     return { type, produce };
 }
