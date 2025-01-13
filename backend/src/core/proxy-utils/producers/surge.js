02946ec81c0423bcd13dea241fe5752c3c48a3c9
xream <xream@icloud.com>
2025-01-13 14:00:38 +0800

    feat: Surge 默认开启 Shadowsocks 2022

--- backend/src/core/proxy-utils/producers/surge.js
+++ backend/src/core/proxy-utils/producers/surge.js
@@ -53,7 +53,7 @@ export default function Surge_Producer() {
     return { produce };
 }
 
-function shadowsocks(proxy, includeUnsupportedProxy) {
+function shadowsocks(proxy) {
     const result = new Result(proxy);
     result.append(`${proxy.name}=${proxy.type},${proxy.server},${proxy.port}`);
     if (!proxy.cipher) {
@@ -87,9 +87,8 @@ function shadowsocks(proxy, includeUnsupportedProxy) {
             'chacha20',
             'chacha20-ietf',
             'none',
-            ...(includeUnsupportedProxy
-                ? ['2022-blake3-aes-128-gcm', '2022-blake3-aes-256-gcm']
-                : []),
+            '2022-blake3-aes-128-gcm',
+            '2022-blake3-aes-256-gcm',
         ].includes(proxy.cipher)
     ) {
         throw new Error(`cipher ${proxy.cipher} is not supported`);
