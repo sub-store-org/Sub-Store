2a76eb64625efd78ac11ff8acdea9f1719204cb4
xream <xream@icloud.com>
2025-02-02 18:56:56 +0800

    feat: mihomo snell 版本小于 3 的节点, 强制去除 udp 字段, 防止内核报错

--- backend/src/core/proxy-utils/producers/clash.js
+++ backend/src/core/proxy-utils/producers/clash.js
@@ -81,6 +81,8 @@ export default function Clash_Producer() {
                     proxy['preshared-key'] =
                         proxy['preshared-key'] ?? proxy['pre-shared-key'];
                     proxy['pre-shared-key'] = proxy['preshared-key'];
+                } else if (proxy.type === 'snell' && proxy.version < 3) {
+                    delete proxy.udp;
                 } else if (proxy.type === 'vless') {
                     if (isPresent(proxy, 'sni')) {
                         proxy.servername = proxy.sni;
