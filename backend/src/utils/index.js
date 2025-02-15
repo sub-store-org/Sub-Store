879b728600619ff22dbf543c9dec8e9964aa3969
xream <xream@icloud.com>
2025-02-16 05:05:33 +0800

    fix: UUID

--- backend/src/utils/index.js
+++ backend/src/utils/index.js
@@ -120,7 +120,7 @@ function numberToString(value) {
 function isValidUUID(uuid) {
     return (
         typeof uuid === 'string' &&
-        /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/.test(
+        /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/.test(
             uuid,
         )
     );
