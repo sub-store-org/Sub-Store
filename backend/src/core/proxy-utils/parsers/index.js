0d2920fadd5a74e1021924c80387d7f56850d3d0
xream <xream@icloud.com>
2025-04-27 09:45:14 +0800

    feat: 兼容 Shadowrocket 非标 VMess URI 输入中的 peer(sni)

--- backend/src/core/proxy-utils/parsers/index.js
+++ backend/src/core/proxy-utils/parsers/index.js
@@ -456,8 +456,12 @@ function URI_VMess() {
                 );
             }
             // https://github.com/2dust/v2rayN/wiki/%E5%88%86%E4%BA%AB%E9%93%BE%E6%8E%A5%E6%A0%BC%E5%BC%8F%E8%AF%B4%E6%98%8E(ver-2)
-            if (proxy.tls && params.sni && params.sni !== '') {
-                proxy.sni = params.sni;
+            if (proxy.tls) {
+                if (params.sni && params.sni !== '') {
+                    proxy.sni = params.sni;
+                } else if (params.peer && params.peer !== '') {
+                    proxy.sni = params.peer;
+                }
             }
             let httpupgrade = false;
             // handle obfs
